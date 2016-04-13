502d6c40f358d966c88536900b963354b7fe6877
smorenoUSAL <s.moreno@usal.edu.ar>
2016-04-13 17:35:21 -0300

    Added files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+package visual;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new Principal();
+		frame.setBounds(100, 100, 650, 450);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+
+}
